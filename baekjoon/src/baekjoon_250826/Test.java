58acd552f907227b14c13b1306ce540c2fea97a8
kosehyeon-byte <rnoc987@naver.com>
2025-09-01 18:00:38 +0900

    250901 update

---
+++ baekjoon/src/baekjoon_250826/Test.java
@@ -0,0 +1,11 @@
+package baekjoon_250826;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// it's for test anything.
+
+
+	}
+
+}
---
+++ baekjoon/src/baekjoon_250901/No9086.java
@@ -0,0 +1,24 @@
+package baekjoon_250901;
+
+import java.util.Scanner;
+
+public class No9086 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String[] arr = new String[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.next();
+		}
+		
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i].charAt(0));
+			System.out.println(arr[i].charAt(arr[i].length()-1));
+		}
+		
+		sc.close();
+	}
+
+}
